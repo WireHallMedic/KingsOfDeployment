381c8be0039473d47849b891ac5c9bbe486952c8
Michael <MichaelWidler@yahoo.com>
2024-10-16 10:31:50 -0700

    Refactor control panel.

--- KoD/KoD/UnitPanel.java
+++ KoD/KoD/UnitPanel.java
@@ -8,18 +8,53 @@ public class UnitPanel extends JPanel implements KoDConstants
    private Unit curUnit;
    private JComboBox<UnitType> unitTypeDD;
    private JComboBox<UnitSize> unitSizeDD;
+   private JTextField nameF;
+   private JTextField infoF;
+   private JPanel[] subpanel;
+   private static final int CONTROL_ROWS = 6;
    
    public UnitPanel()
    {
       super();
       curUnit = new Unit();
-      JPanel subpanel0 = new JPanel();
-      JPanel subpanel1 = new JPanel();
+      JPanel leftPanel = new JPanel();
+      JPanel rightPanel = new JPanel();
       setLayout(new GridLayout(1, 2));
-      subpanel0.setBackground(Color.RED);
-      subpanel1.setBackground(Color.BLUE);
-      add(subpanel0);
-      add(subpanel1);
+      leftPanel.setBackground(Color.GREEN);
+      add(leftPanel);
+      add(rightPanel);
+      rightPanel.setLayout(new GridLayout(CONTROL_ROWS, 1));
+      
+      subpanel = new JPanel[CONTROL_ROWS];
+      for(int i = 0; i < CONTROL_ROWS; i++)
+      {
+         subpanel[i] = new JPanel();
+         rightPanel.add(subpanel[i]);
+      }
+      
+      subpanel[0].setLayout(new GridLayout(1, 2));
+      subpanel[0].add(new JLabel("Name"));
+      nameF = new JTextField(curUnit.getName());
+      subpanel[0].add(nameF);
+      
+      subpanel[1].setLayout(new GridLayout(1, 2));
+      subpanel[1].add(new JLabel("Unit Type"));
+      unitTypeDD = new JComboBox<UnitType>(UnitType.values());
+      subpanel[1].add(unitTypeDD);
+      
+      subpanel[2].setLayout(new GridLayout(1, 2));
+      subpanel[2].add(new JLabel("Unit Size"));
+      unitSizeDD = new JComboBox<UnitSize>(UnitSize.values());
+      subpanel[2].add(unitSizeDD);
+      
+      subpanel[3].setLayout(new GridLayout(1, 2));
+      subpanel[3].add(new JLabel("Decoration"));
+      
+      subpanel[4].setLayout(new GridLayout(1, 1));
+      infoF = new JTextField("infoF");
+      infoF.setEditable(false);
+      infoF.setFocusable(false);
+      subpanel[4].add(infoF);
       this.repaint();
    }
    
