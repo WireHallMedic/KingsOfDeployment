24910a94b593452772ab9fc4137f1c69e697b514
Michael <MichaelWidler@yahoo.com>
2024-10-16 08:53:21 -0700

    Roll individual dimensions into enum.

--- KoD/KoD/KoDConstants.java
+++ KoD/KoD/KoDConstants.java
@@ -2,8 +2,31 @@ package KoD;
 
 public interface KoDConstants
 {
-   public enum UnitType {INFANTRY, HEAVY_INFANTRY, CAVALRY, LARGE_INFANTRY, SWARM, LARGE_CAVALRY, MONSTROUS_INFANTRY, CHARIOT,
-                         MONSTER, WAR_MACHINE, TITAN};
+   public enum UnitType 
+   {
+      INFANTRY             ("Infantry", 20, 20), 
+      HEAVY_INFANTRY       ("Heavy Infantry", 25, 25), 
+      CAVALRY              ("Cavalry", 25, 50), 
+      LARGE_INFANTRY       ("Large Infantry", 40, 40), 
+      SWARM                ("Swarm", 40, 40), 
+      LARGE_CAVALRY        ("Large Cavalry", 50, 50), 
+      MONSTROUS_INFANTRY   ("Monstrous Infantry", 50, 50), 
+      CHARIOT              ("Chariot", 50, 100), 
+      MONSTER              ("Monster", 50, 50), 
+      WAR_MACHINE          ("War Machine", 50, 50), 
+      TITAN                ("Titan", 75, 75);
+      
+      public String name;
+      public int width;
+      public int length;
+      
+      private UnitType(String n, int w, int l)
+      {
+         name = n;
+         width = w;
+         length = l;
+      }
+   };
    
    public enum UnitSize {INDIVIDUAL, TROOP, REGIMENT, HORDE, LEGION};
    
