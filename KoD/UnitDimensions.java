096ec69f21654c2f51754725f5afa0000f2cacac
Michael <MichaelWidler@yahoo.com>
2024-10-16 06:47:46 -0700

    Add unit dimension interface.

---
+++ KoD/UnitDimensions.java
@@ -0,0 +1,17 @@
+package KoD
+
+public interface UnitDimensions
+{
+   public static final int INFANTRY = 0;
+   public static final int HEAVY_INFANTRY = 1;
+   public static final int CAVALRY = 2;
+   public static final int LARGE_INFANTRY = 3;
+   public static final int SWARM = 3;
+   public static final int LARGE_CAVALRY = 4;
+   public static final int MONSTROUS_INFANTRY = 4;
+   public static final int CHARIOT = 5;
+   public static final int MONSTER = 6;
+   public static final int WAR_MACHINE = 6;
+   public static final int TITAN = 7;
+}
+
